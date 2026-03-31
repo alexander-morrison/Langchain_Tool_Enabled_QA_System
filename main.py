c8e8e1a18a6bb0073960af4a44e2bbae0cfbac40
Alexander Morrison <1611.developer@gmail.com>
2026-03-31 10:51:46 -0700

    working on setup

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-# this is the main file
+import os
+import dotenv
+from langcore_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
+llm = ChatOpenAI(api_key=os.getenv("OPENAI_API_KEY"))
